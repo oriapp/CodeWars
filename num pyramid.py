3414cc337af4847fcf0267bd1f122762b0e4ee71
oriapp <plbwymw@gmail.com>
2021-12-13 18:40:40 +0200

    num pyramid

---
+++ num pyramid.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+for i in range(1, n+1):
+    print(str(n) * i)
