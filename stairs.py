3344774a4dacf382284a3e3dad554a43a7a919b6
R C N <plbwymw@gmail.com>
2022-05-09 22:44:18 +0300

    Update stairs.py

--- stairs.py
+++ stairs.py
@@ -1,3 +1,8 @@
-for i in range(1, int(input())+1):
-    print(" " * (i - 1), end="")
-    print("*" * i)
+def solve():
+    for i in range(1, int(input())+1):
+        print(f"{' ' * (i - 1)} {'-' * i}")
+    print(chr_counter("ass"))
+
+
+if __name__ == "__main__":
+    solve()
